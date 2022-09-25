ceb3829135fdcb6b539c5bd50d6feeed9792b181
farhandito15 <farhandito15@gmail.com>
2022-09-25 17:17:03 +0700

    Switch bahasa maksudnya mas ?

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form;
+use App\Http\Requests\StoreFormRequest;
+use App\Http\Requests\UpdateFormRequest;
+use Illuminate\Support\Facades\Session;
+
+class FormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        if ($locale = session('locale')) {
+            app()->setLocale($locale);
+        }
+        return view('checkout.index');
+    }
+
+    public function switch($locale)
+    {
+        Session::put('locale', $locale);
+
+        return redirect()->back();
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreFormRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreFormRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateFormRequest  $request
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateFormRequest $request, Form $form)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Form $form)
+    {
+        //
+    }
+}
---
+++ lang/id/form.php
@@ -0,0 +1,8 @@
+<?php 
+
+return [
+    'first_name' => 'Nama depan',
+    'last_name' => 'Nama belakang',
+    'judul' => 'Form Pengisian'
+]
+ ?>
